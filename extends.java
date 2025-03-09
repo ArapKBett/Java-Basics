015f07f9d79eebccadaec9b6f2de59fcd8ef40ed
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-03-09 10:06:27 -0400

    Create extends.java

---
+++ extends.java
@@ -0,0 +1,11 @@
+class ParentClass {
+    void display() {
+        System.out.println("This is the parent class.");
+    }
+}
+
+class ChildClass extends ParentClass {
+    void show() {
+        System.out.println("This is the child class.");
+    }
+}
